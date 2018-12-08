3ae22c71bd3e28b20650da38eef48e6d9a7f4d6e
Matheus Santos <tetofsa15@gmail.com>
2018-12-08 18:15:02 -0300

    a

--- app1/index.ios.js
+++ app1/index.ios.js
@@ -1,11 +1,23 @@
 var React = require('react'); //modulo react
 var Text = require('react-native').Text;
 var AppRegistry = require('react-native').AppRegistry;
+var View = require('react-native').View;
+var Button = require('react-native').Button;
 /*() => { return App} é a msm coisa  que () => App*/
+const gerarNumeroAleatorio = () => {
+  var numero_aleatorio = Math.random()*10;
+  alert(numero_aleatorio);
+}
 const App = () => {   
   return (
       //JSX, uma forma de representação dos objetos em um formato de componentes
-      <Text>Meu primeiro APP</Text> 
+      <View>
+        <Text>Meu primeiro APP</Text> 
+        <Button 
+          title="Gera um número randômico"
+          onPress={gerarNumeroAleatorio}
+         /> 
+      </View>
     );
 };
 

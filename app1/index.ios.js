a9c178ac4b70d0dcce06e6dab84c497d652aa420
Matheus Santos <tetofsa15@gmail.com>
2018-12-08 18:16:38 -0300

    a

--- app1/index.ios.js
+++ app1/index.ios.js
@@ -5,14 +5,14 @@ var View = require('react-native').View;
 var Button = require('react-native').Button;
 /*() => { return App} é a msm coisa  que () => App*/
 const gerarNumeroAleatorio = () => {
-  var numero_aleatorio = Math.random()*10;
+  var numero_aleatorio = Math.floor(Math.random()*10);
   alert(numero_aleatorio);
 }
 const App = () => {   
   return (
       //JSX, uma forma de representação dos objetos em um formato de componentes
       <View>
-        <Text>Meu primeiro APP</Text> 
+        <Text>Gerador de Números Aleatórios</Text> 
         <Button 
           title="Gera um número randômico"
           onPress={gerarNumeroAleatorio}

75e809d9871c1952fbefbb6149d04280e72da514
Matheus Santos <tetofsa15@gmail.com>
2018-12-08 17:53:52 -0300

    a

--- app1/index.ios.js
+++ app1/index.ios.js
@@ -1,10 +1,12 @@
 var React = require('react'); //modulo react
 var Text = require('react-native').Text;
 var AppRegistry = require('react-native').AppRegistry;
+
 const App = function(){
+
   return (
       <Text>Meu primeiro APP</Text>
     );
 };
 
-AppRegistry.register Component('app1', function(){ return App;});
+AppRegistry.registerComponent('app1', function(){ return App});

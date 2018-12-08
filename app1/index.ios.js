72847a112ee62fa03decf556bc8cb4b70418a4c4
Matheus Santos <tetofsa15@gmail.com>
2018-12-08 18:08:31 -0300

    arrow function

--- app1/index.ios.js
+++ app1/index.ios.js
@@ -2,7 +2,7 @@ var React = require('react'); //modulo react
 var Text = require('react-native').Text;
 var AppRegistry = require('react-native').AppRegistry;
 /*() => { return App} é a msm coisa  que () => App*/
-const App = () =>   
+const App = () => {   
   return (
       //JSX, uma forma de representação dos objetos em um formato de componentes
       <Text>Meu primeiro APP</Text> 

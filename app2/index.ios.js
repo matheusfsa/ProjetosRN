861f399eb6c97f7f26da7051ea0c263e64c6f1d4
Matheus Santos <tetofsa15@gmail.com>
2018-12-09 15:24:07 -0300

    s7a93

--- app2/index.ios.js
+++ app2/index.ios.js
@@ -1,30 +1,36 @@
 //Import
 import React from 'react';
-import {Text,View, AppRegistry, Image} from 'react-native';
+import {Text,View, AppRegistry, Image, TouchableOpacity} from 'react-native';
 
 //Formatações
 const Estilos = {
   principal:{
-    paddingTop:40
-  }, 
-  imagem:{
-    justifyContent:'flex-end',
-    paddingBottom: 5
-  }    
-
-
+    flex:1,
+    justifyContent:'center',
+    alignItems:'center'
+  },
+  botao: {
+    backgroundColor: '#538530',
+    paddingHorizontal:40,
+    paddingVertical: 10,
+    marginTop: 20
+  },
+  textoBotao: {
+    color: 'white',
+    fontSize: 16,
+    fontWeight: 'bold'
+  }
 };
 const botaoPressionado = () => {
-  alert('Botao Pressionado')
-}
 //Criar o Componente
 const App2 = () => {
-  const {principal,imagem} = Estilos;
+  const {principal, botao, textoBotao} = Estilos;
   return (
     <View style={principal}>
-      <Image style={imagem} source={require('./imgs/uvas.png')}>
-        <Text>Legenda</Text>
-      </Image>
+      <Image source={require('./imgs/logo.png')}/>
+      <TouchableOpacity style={botao}>
+        <Text style={textoBotao}> Nova Frase</Text>
+      </TouchableOpacity>
     </View>
   );
 };

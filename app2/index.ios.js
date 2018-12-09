865cf280c14c455121ee80467527757a4e6df9dc
Matheus Santos <tetofsa15@gmail.com>
2018-12-09 15:30:50 -0300

    s7a94

--- app2/index.ios.js
+++ app2/index.ios.js
@@ -21,14 +21,23 @@ const Estilos = {
     fontWeight: 'bold'
   }
 };
-const botaoPressionado = () => {}
+const gerarNovaFrase = () => {
+  var i = Math.floor(Math.random()*4);
+  var frases = ["Relacionamento é para ter paz. Se for para passar nervoso, prefiro dobrar lençol com elástico.", 
+              "Se a pessoa me oferecer cachaça, a amizade acaba...de começar.",
+              "Me diga com quem tu andas, e eu te direi se irei junto."
+              "Estou quase morando sozinho. Só falta a minha família se mudar."];
+  alert(frases[i])
+}
 //Criar o Componente
 const App2 = () => {
   const {principal, botao, textoBotao} = Estilos;
   return (
     <View style={principal}>
       <Image source={require('./imgs/logo.png')}/>
-      <TouchableOpacity style={botao}>
+      <TouchableOpacity 
+        onPress={gerarNovaFrase}
+        style={botao}>
         <Text style={textoBotao}> Nova Frase</Text>
       </TouchableOpacity>
     </View>

717629413eb71149a8f7653f86646b0eb16cf461
Matheus Santos <tetofsa15@gmail.com>
2018-12-09 14:52:53 -0300

    s7a88

--- app2/index.ios.js
+++ app2/index.ios.js
@@ -4,45 +4,20 @@ import {Text,View, AppRegistry} from 'react-native';
 
 //Formatações
 const Estilos = {
-  estiloTexto: {
-    fontSize: 30,
-    backgroundColor: '#08509B',
-    paddingTop:15,
-    color: '#fff',
-    textAlign: 'justify'
-    //textDecoration: 'underline'
-    //fontWeight: 'bold'
-    //fontStyle: 'italic',
-    //shadowColor: '#000000',
-    //shadowOffset: {width:0, height:2},
-    //shadowOpacity: 1,
-    //shadowRadius: 3
-    //Largura e Altura(em dp): width e height
-    //espaçamento interno: padding{Top,Botton, Left, Right, Vertical, Horizontal}
-    //espaçamento externo: margin{Top,Botton, Left, Right, Vertical, Horizontal}
-  },
-  estiloTexto2: {
-    fontSize: 30,
-    backgroundColor: '#08509B',
-    paddingTop:15,
-    color: '#fff',
-    textAlign: 'justify'
-  }, 
-
-  estiloView: {
-    flex: 1, //1:1
-    backgroundColor: 'skyblue'
-
-  }
+  principal:{
+    paddingTop:40
+  }    
 
 };
 
 //Criar o Componente
 const App2 = () => {
-  const {estiloTexto, estiloView} = Estilos;
+  const {principal} = Estilos;
   return (
-    <View style={estiloView}>
-      <Text style={estiloTexto}> "Frases do dia"</Text>
+    <View style={principal}>
+      <Button 
+        title='Clique Aqui'
+      />
     </View>
   );
 };

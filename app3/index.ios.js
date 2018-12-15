e5bd7de2f71839cb48a37145cba50f7f8976e007
Matheus Santos <tetofsa15@gmail.com>
2018-12-15 13:36:34 -0300

    s8a102

--- app3/index.ios.js
+++ app3/index.ios.js
@@ -16,24 +16,54 @@ import {
 export default class app3 extends Component {
   constructor(props){
     super(props);
-    this.state = {escolhaUsuario:"", escolhaComputador:""}
+    this.state = {escolhaUsuario:"", 
+                  escolhaComputador:"", 
+                  resultado:""}
   }
   jokenpo(escolhaUsuario){
     var escolhaComputador = ["pedra", "papel", "tesoura"];
     var numAleatorio = Math.floor(Math.random()*3);
+    
+    var resultado = "";
+    if(escolhaComputador == "pedra"){
+      if(escolhaUsuario == "pedra"){
+        resultado = "Empate";
+      } else if(escolhaUsuario == "papel"){
+        resultado = "Vitória";
+      } else {
+        resultado = "Derrota";
+      }
+    }else if(escolhaComputador == "papel"){
+      if(escolhaUsuario == "pedra"){
+        resultado = "Derrota";
+      } else if(escolhaUsuario == "papel"){
+        resultado = "Empate";
+      } else {
+        resultado = "Vitória";
+      }
+    } else {
+      if(escolhaUsuario == "pedra"){
+        resultado = "Vitória";
+      } else if(escolhaUsuario == "papel"){
+        resultado = "Derrota";
+      } else {
+        resultado = "Empate";
+      }
+    }
     this.setState({ escolhaUsuario:escolhaUsuario,
                     escolhaComputador : escolhaComputador[numAleatorio]
-                    });
+                    resultado: resultado});
   }
+
   render(){
     return(
       <View>
         <Text> computador: {this.state.escolhaComputador}</Text>
         <Text> user: {this.state.escolhaUsuario}</Text>
-        <Text> res </Text>
+        <Text> res: {this.state.resultado} </Text>
         <Button title="pedra" onPress={() => {this.jokenpo('pedra')}}/>
-        <Button title="papel"onPress={() => {this.jokenpo('papel')}}/>
-        <Button title="tesoura"onPress={() => {this.jokenpo('tesoura')}}/>
+        <Button title="papel" onPress={() => {this.jokenpo('papel')}}/>
+        <Button title="tesoura" onPress={() => {this.jokenpo('tesoura')}}/>
       </View>
       );
   }

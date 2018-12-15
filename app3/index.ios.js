d2d7bf7c75685a615d8ac1679dd9493e0fdcd9f0
Matheus Santos <tetofsa15@gmail.com>
2018-12-15 10:56:09 -0300

    s8a100

--- app3/index.ios.js
+++ app3/index.ios.js
@@ -9,45 +9,35 @@ import {
   AppRegistry,
   StyleSheet,
   Text,
-  View
+  View,
+  Button
 } from 'react-native';
-
-export default class app3 extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
+class MeuComponente extends Component{
+  render(){
+    return(
+      <View>
+        <Text>{this.props.teste}</Text>
       </View>
-    );
+      )
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
+export default class app3 extends Component {
+  constructor(props){
+    super(props);
+    this.state = {texto: 'Texto teste'};
+  }
+  alteraTexto(){
+    this.setState({texto: 'Outra Coisa'});
+  }
+  render(){
+    return(
+      <MeuComponente teste={this.state.texto}></MeuComponente>
+      <Button
+       title = 'Botao'
+       onPress={() => {this.alteraTexto()}}
+      />
+      );
+  }
+}
 
 AppRegistry.registerComponent('app3', () => app3);

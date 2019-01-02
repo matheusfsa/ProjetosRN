17c31b70032ea1fcf27e5feafff6e94be826f5f5
Matheus Santos <tetofsa15@gmail.com>
2019-01-02 14:52:24 -0300

    s9a123

--- app4/src/components/itens.js
+++ app4/src/components/itens.js
@@ -2,19 +2,46 @@ import React, { Component } from 'react';
 import {
   Text,
   View,
-  Image
+  Image,
+  StyleSheet
 } from 'react-native';
 
 export default class Itens extends Component {
   render() {
     return (
-    	<View>
-    		//<Image style={{height: 100, width: 100}}source={{uri: this.props.item.foto}} />
-	    	<Text>{this.props.item.titulo}</Text>
+    	<View style={styles.item}>
+    		<View style={styles.foto}>
+    			<Image style={{height: 100, width: 100}}source={{uri: this.props.item.foto}} />
+	    	</View>
+	    	<View style={styles.detalhesItem}>
+	    	<Text style={styles.txtTitulo}>{this.props.item.titulo}</Text>
 	    	<Text>{this.props.item.valor}</Text>
 	    	<Text>{this.props.item.local_anuncio}</Text>
 	    	<Text>{this.props.item.data_publicacao}</Text>
+    		</View>
     	</View>
     );
   }
 }
+const style = StyleSheet.create({
+	item:{
+		borderWidth: 0.5,
+		borderColor:'#999',
+		margin: 10,
+		padding: 10,
+		flexDirection: 'row'
+	},
+	foto:{
+		width:102,
+		height:102
+	}
+	detalhesItem:{
+		marginLeft:20
+	}
+	txtTitulo: {
+		fontSize:18,
+		color:'blue',
+		marginBottom: 5
+	}
+
+});

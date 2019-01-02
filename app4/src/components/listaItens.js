ffdd6ce219b9ab5ce6d9ffbe05ee3a3dea96c7db
Matheus Santos <tetofsa15@gmail.com>
2019-01-02 12:58:55 -0300

    s9a120

--- app4/src/components/listaItens.js
+++ app4/src/components/listaItens.js
@@ -5,14 +5,19 @@ import {
 import axios from 'axios';
 import Itens from './itens.js';
 export default class ListaItens extends Component {
+  constructor(props){
+  	super(props);
+  	this.state = { listaItens: []};
+  }
   componentWillMount(){
   	axios.get('http://faus.com.br/recursos/c/dmairr/api/itens.html')
-  	.then( response => { console.log(response); })
+  	.then( response => { this.setState({listaItens: response.data}); })
   	.cacth(() => {console.log('Erro'); });
   }
   render() {
     return (
     	<View>
+    		{this.state.listaItens.map(item => {<Text key={item.titulo}>{item.titulo}</Text>})}
      		<Itens/>
      		<Itens/>
      	<View>

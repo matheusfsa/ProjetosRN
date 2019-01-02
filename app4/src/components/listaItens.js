02d79203d35a6be9c22fd01032fa75b7e08aeb04
Matheus Santos <tetofsa15@gmail.com>
2019-01-02 14:33:22 -0300

    s9a122

--- app4/src/components/listaItens.js
+++ app4/src/components/listaItens.js
@@ -11,11 +11,11 @@ export default class ListaItens extends Component {
   	super(props);
   	this.state = { listaItens: []};
   }
-  componentWillMount(){
-  	axios.get('http://faus.com.br/recursos/c/dmairr/api/itens.html')
-			.then(response => { this.setState({ listaItens: response.data }); })
-			.catch(() => { console.log('Erro ao recuperar os dados'); });
-  }
+  //componentWillMount(){
+  	//axios.get('http://faus.com.br/recursos/c/dmairr/api/itens.html')
+			//.then(response => { this.setState({ listaItens: response.data }); })
+			//.catch(() => { console.log('Erro ao recuperar os dados'); });
+  //}
   render() {
   	this.setState({listaItens:[{ "foto" : "http://faus.com.br/recursos/c/dmairr/imgs/mesa_vidro.jpg", "titulo" : "Mesa de vidro para computador", "valor": 160, "local_anuncio": "Santo AndrÃ©", "data_publicacao": "01/12/2016" }]});
     return (

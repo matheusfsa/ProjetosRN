3251632876d19bab1f1b30c7f8c84e310fd64bc4
Matheus Santos <tetofsa15@gmail.com>
2019-01-02 12:37:39 -0300

    s9a119

--- app4/src/components/listaItens.js
+++ app4/src/components/listaItens.js
@@ -1,12 +1,21 @@
 import React, { Component } from 'react';
 import {
-  Text
+  View
 } from 'react-native';
-
+import axios from 'axios';
+import Itens from './itens.js';
 export default class ListaItens extends Component {
+  componentWillMount(){
+  	axios.get('http://faus.com.br/recursos/c/dmairr/api/itens.html')
+  	.then( response => { console.log(response); })
+  	.cacth(() => {console.log('Erro'); });
+  }
   render() {
     return (
-     	<Text>Teste</Text>
+    	<View>
+     		<Itens/>
+     		<Itens/>
+     	<View>
     );
   }
 }

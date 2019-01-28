2257f0fc59f9a7aa94900abaa07fb4d85b74b7c7
Matheus Santos <tetofsa15@gmail.com>
2019-01-28 20:46:42 -0300

    s11a153

--- app6/__tests__/index.android.js
+++ app6/__tests__/index.android.js
@@ -1,15 +1,12 @@
-/**
- * @format
- * @lint-ignore-every XPLATJSCOPYRIGHT1
- */
-
 import 'react-native';
 import React from 'react';
-import App from '../App';
+import Index from '../index.android.js';
 
 // Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
 
 it('renders correctly', () => {
-  renderer.create(<App />);
+  const tree = renderer.create(
+    <Index />
+  );
 });

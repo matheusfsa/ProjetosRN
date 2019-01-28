382e147397b57a84a120866e8de546a8bb327e86
Matheus Santos <tetofsa15@gmail.com>
2019-01-28 19:01:45 -0300

    s11a153

--- app6/App.js
+++ app6/App.js
@@ -1,49 +1,20 @@
 import React, { Component } from 'react';
 import {
-  StyleSheet,
-  Text,
-  View,
-  Image
+ View
 } from 'react-native';
-
-const logo = require('./src/imgs/logo.png');
-const btnJogar = require('./src/imgs/botao_jogar.png');
-const btnSobreJogo = require('./src/imgs/sobre_jogo.png');
-const btnOutrosJogos = require('./src/imgs/outros_jogos.png');
-
+import {Router, Scene} from 'react-native-router-flux';
+import CenaPrincipal from './src/components/CenaPrincipal';
+import SobreJogo from './src/components/SobreJogo';
+import OutrosJogos from './src/components/OutrosJogos';
 export default class App extends Component {
   render() {
     return (
-      <View style={styles.cenaPrincipal}>
-
-        <View style={styles.apresentacaoJogo}>
-          <Image source={logo} />
-          <Image source={btnJogar} />
-        </View>
-
-        <View style={styles.rodape}>
-          <Image source={btnSobreJogo} />
-          <Image source={btnOutrosJogos} />
-        </View>
-
-      </View>
+      <Router>
+        <Scene key='principal' component={CenaPrincipal} initil title="Cara ou Coroa"/>
+        <Scene key='sobrejogo' component={SobreJogo}/>
+        <Scene key='outrosjogos' component={OutrosJogos}/>
+      </Router>
     );
   }
 }
 
-const styles = StyleSheet.create({
-  cenaPrincipal: {
-    flex: 1,
-    backgroundColor: '#61BD8C'
-  },
-  apresentacaoJogo: {
-    flex: 10,
-    justifyContent: 'center',
-    alignItems: 'center'
-  },
-  rodape: {
-    flex: 1,
-    flexDirection: 'row',
-    justifyContent: 'space-between'
-  }
-});

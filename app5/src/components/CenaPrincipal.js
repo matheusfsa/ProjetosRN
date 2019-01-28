45fa3f89fa318924a277142bf777384a8d7f8e6c
Matheus Santos <tetofsa15@gmail.com>
2019-01-28 18:24:16 -0300

    s11a149

--- app5/src/components/CenaPrincipal.js
+++ app5/src/components/CenaPrincipal.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import {StatusBar, Text, View} from 'react-native';
+import {StatusBar,StyleSheet, Text, View} from 'react-native';
 
 import BarraNavegacao from "./BarraNavegacao";
 const logo = require('../imgs/logo.png');
@@ -15,12 +15,35 @@ export default class CenaPrincipal extends Component<Props> {
           backgroundColor='#CCC'
          />
         <BarraNavegacao/>
-        <Image source={logo}/>
-        <Image source={menu_cliente}/>
-        <Image source={menu_contato}/>
-        <Image source={menu_empresa}/>
-        <Image source={menu_servico}/>
+        <View style={styles.logo}>
+          <Image source={logo}/>
+        </View>
+        <View style={styles.menuGrupo}>
+          <View style={styles.menu}>
+            <Image style={styles.imgMenu} source={menu_cliente}/>
+            <Image style={styles.imgMenu} source={menu_contato}/>
+          </View>
+          <View style={styles.menuGrupo}>
+            <Image style={styles.imgMenu} source={menu_empresa}/>
+            <Image style={styles.imgMenu} source={menu_servico}/>
+          </View>
+        </View>
       </View>  
     );
 }
+const styles = StyleSheet.create({
+  logo:{
+    marginTOp: 30,
+    alignItems: 'center'
+  },
+  menu: {
+    alignItems: 'center' 
+  },
+  menuGrupo: {
+    flexDirection: 'row' 
+  },
+  imgMenu:{
+     margin: 15 
+  }
+});
 

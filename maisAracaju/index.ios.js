daaee2cc05ae09073293a81de952e25b2c889df9
Matheus Santos <tetofsa15@gmail.com>
2019-02-16 11:59:24 -0300

    mais_aracaju

---
+++ maisAracaju/index.ios.js
@@ -0,0 +1,21 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  AppRegistry
+} from 'react-native';
+import Principal from ".src/principal";
+export default class maisAracaju extends Component {
+  render() {
+    return (
+      <Principal />
+    );
+  }
+}
+
+
+AppRegistry.registerComponent('maisAracaju', () => maisAracaju);
---
+++ maisAracaju/src/components/principal.js
@@ -0,0 +1,72 @@
+import React, { Component } from 'react';
+import {
+  TouchableHighlight,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+
+export default class Principal extends Component {
+	_onPressButton() {
+    	Alert.alert('You tapped the button!')
+  	}
+	render() {
+	    return (
+		    <View style={styles.container}>
+		      	<View style={styles.cadastro}>
+			        <TouchableHighlight onPress={this._onPressButton} underlayColor="white">
+			          	<View style={styles.button}>
+			            	<Text style={styles.buttonText}>Cadastre-se</Text>
+			          	</View>
+		        	</TouchableHighlight>
+		        </View>
+	        	<View style={styles.menuGrupo}>
+	        		<View style={styles.menuGrupo}>
+				        <TouchableHighlight onPress={this._onPressButton} underlayColor="white">
+				          	<View style={styles.button}>
+				            	<Text style={styles.buttonText}>Quem Somos?</Text>
+				          	</View>
+			        	</TouchableHighlight>
+				        <TouchableHighlight onPress={this._onPressButton} underlayColor="white">
+				          	<View style={styles.button}>
+				            	<Text style={styles.buttonText}>Como Funciona?</Text>
+				          	</View>
+			        	</TouchableHighlight>
+			        </View>
+			        <View style={styles.menuGrupo}>
+				        <TouchableHighlight onPress={this._onPressButton} underlayColor="white">
+				          	<View style={styles.button}>
+				            	<Text style={styles.buttonText}>Sugestões</Text>
+				          	</View>
+			        	</TouchableHighlight>
+				        <TouchableHighlight onPress={this._onPressButton} underlayColor="white">
+				          	<View style={styles.button}>
+				            	<Text style={styles.buttonText}>Notícias</Text>
+				          	</View>
+			        	</TouchableHighlight>
+		        	</View>
+	        	</View>
+	        </View>
+	    );
+	  }
+}
+
+const styles = StyleSheet.create({
+  cadastro:{
+    marginTop: 30,
+    alignItems: 'center'
+  },
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  menu: {
+    alignItems: 'center' 
+  },
+  menuGrupo: {
+    flexDirection: 'row' 
+  }
+
+});

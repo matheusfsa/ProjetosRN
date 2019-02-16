53d941d213b9a1147fc2a90967314befef4c1b27
Matheus Santos <tetofsa15@gmail.com>
2019-02-16 14:21:23 -0300

    mais_aracaju

--- maisAracaju/src/components/Principal.js
+++ maisAracaju/src/components/Principal.js
@@ -56,6 +56,7 @@ const styles = StyleSheet.create({
    button: {
     marginBottom: 30,
     width: 100,
+    height: 50,
     alignItems: 'center',
     borderRadius:10,
     backgroundColor: '#2196F3'
